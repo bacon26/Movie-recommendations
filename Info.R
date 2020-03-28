38df927dde7a8427cd8a39b69db8a52cc3b974ff
bacon26 <daniel.m.nilsson@outlook.com>
2020-03-28 18:13:43 +0100

    add Info.R

---
+++ Info.R
@@ -0,0 +1,29 @@
+##### Movie recommendations
+
+### Types
+# Recommender systems använder sig oftast av ena eller båda av dessa tekniker:
+# - Collaborative filtering (CF) [Liknande användare]
+# - Content based filtering (CBF) [Liknande produkt-egenskaper]
+# CF bygger en model utifrån användarens tidigare beteende samt beteende
+# hos liknande användare. Rekommenderar produkter som liknande användare har gillat.
+# CBF använder fördefinierade egenskaper och rekommenderar produkter med liknande
+# egenskaper som användaren tidigare gillat.
+# Last.fm vs Pandora
+# https://en.wikipedia.org/wiki/Recommender_system 
+
+### Case: Last.FM Music
+## Collaborative filtering
+# - User based
+# - Item based
+
+### Matrix factorization
+# Matrix factorization is a class of collaborative filtering algorithms used
+# in recommender systems. Matrix factorization algorithms work by decomposing
+# the user-item interaction matrix into the product of two lower dimensionality
+# rectangular matrices. This family of methods became widely known during
+# the Netflix prize challenge.
+# https://www.youtube.com/watch?v=ZspR5PZemcs 
+
+
+### Model-based vs memory-based algorithms?
+# http://www.cs.carleton.edu/cs_comps/0607/recommend/recommender/modelbased.html 
